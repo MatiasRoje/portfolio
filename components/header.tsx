556d01f499ad2516437a5e60900f0afc72808a80
MatiasRoje <rojechi@gmail.com>
2023-11-11 13:57:00 +0100

    intro section done

--- components/header.tsx
+++ components/header.tsx
@@ -15,108 +15,84 @@ import {
 
 function Header() {
   return (
-    <header className="z-50 relative">
+    <header className="relative z-50">
       <motion.div
-        className="fixed top-0 left-1/2 h-12 w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[32rem] sm:rounded-full"
+        className="fixed left-1/2 top-0 h-12 w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[32rem] sm:rounded-full"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+      <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-gray-800 sm:w-[initial] sm:flex-nowrap sm:gap-8">
           <motion.li
-            className="h-3/4 flex items-center justify-center relative"
+            className="relative flex h-3/4 items-center justify-center"
             initial={{ y: -100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
           >
             <Link
               href="#home"
-              className="flex w-full items-center justify-center px-3 py-3"
+              className="flex w-full items-center justify-center px-3 py-3 outline-none transition hover:scale-110 focus:scale-110"
             >
-              <FontAwesomeIcon
-                icon={faHome}
-                style={{ fontSize: 20 }}
-                className="hover:scale-110 transition"
-              />
+              <FontAwesomeIcon icon={faHome} style={{ fontSize: 20 }} />
             </Link>
           </motion.li>
           <motion.li
-            className="h-3/4 flex items-center justify-center relative"
+            className="relative flex h-3/4 items-center justify-center"
             initial={{ y: -100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
           >
             <Link
               href="#about"
-              className="flex w-full items-center justify-center px-3 py-3"
+              className="flex w-full items-center justify-center px-3 py-3 outline-none transition hover:scale-110 focus:scale-110"
             >
-              <FontAwesomeIcon
-                icon={faUser}
-                style={{ fontSize: 20 }}
-                className="hover:scale-110 transition"
-              />
+              <FontAwesomeIcon icon={faUser} style={{ fontSize: 20 }} />
             </Link>
           </motion.li>
           <motion.li
-            className="h-3/4 flex items-center justify-center relative"
+            className="relative flex h-3/4 items-center justify-center"
             initial={{ y: -100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
           >
             <Link
               href="#skills"
-              className="flex w-full items-center justify-center px-3 py-3"
+              className="flex w-full items-center justify-center px-3 py-3 outline-none transition hover:scale-110 focus:scale-110"
             >
-              <FontAwesomeIcon
-                icon={faLightbulb}
-                style={{ fontSize: 20 }}
-                className="hover:scale-110 transition"
-              />
+              <FontAwesomeIcon icon={faLightbulb} style={{ fontSize: 20 }} />
             </Link>
           </motion.li>
           <motion.li
-            className="h-3/4 flex items-center justify-center relative"
+            className="relative flex h-3/4 items-center justify-center"
             initial={{ y: -100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
           >
             <Link
               href="#projects"
-              className="flex w-full items-center justify-center px-3 py-3"
+              className="flex w-full items-center justify-center px-3 py-3 outline-none transition hover:scale-110 focus:scale-110"
             >
-              <FontAwesomeIcon
-                icon={faLaptop}
-                style={{ fontSize: 20 }}
-                className="hover:scale-110 transition"
-              />
+              <FontAwesomeIcon icon={faLaptop} style={{ fontSize: 20 }} />
             </Link>
           </motion.li>
           <motion.li
-            className="h-3/4 flex items-center justify-center relative"
+            className="relative flex h-3/4 items-center justify-center"
             initial={{ y: -100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
           >
             <Link
               href="#contact"
-              className="flex w-full items-center justify-center px-3 py-3"
+              className="flex w-full items-center justify-center px-3 py-3 outline-none transition hover:scale-110 focus:scale-110"
             >
-              <FontAwesomeIcon
-                icon={faEnvelope}
-                style={{ fontSize: 20 }}
-                className="hover:scale-110 transition"
-              />
+              <FontAwesomeIcon icon={faEnvelope} style={{ fontSize: 20 }} />
             </Link>
           </motion.li>
           <motion.li
-            className="h-3/4 flex items-center justify-center relative"
+            className="relative flex h-3/4 items-center justify-center"
             initial={{ y: -100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
           >
             <Link
               href="#"
-              className="flex w-full items-center justify-center px-3 py-3"
+              className="flex w-full items-center justify-center px-3 py-3 outline-none transition hover:scale-110 focus:scale-110"
             >
-              <FontAwesomeIcon
-                icon={faMoon}
-                style={{ fontSize: 20 }}
-                className="hover:scale-110 transition"
-              />
+              <FontAwesomeIcon icon={faMoon} style={{ fontSize: 20 }} />
             </Link>
           </motion.li>
         </ul>
--- components/intro.tsx
+++ components/intro.tsx
@@ -2,13 +2,30 @@
 
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowDown,
+  faArrowRight,
+  faFileArrowDown,
+} from "@fortawesome/free-solid-svg-icons";
+import { faCircleDown } from "@fortawesome/free-solid-svg-icons/faCircleDown";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] scroll-mt-[100rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
-          <motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.3,
+            }}
+          >
             <Image
               src="/portrait.jpg"
               alt="Matías portrait"
@@ -16,12 +33,88 @@ function Intro() {
               height="192"
               quality="95"
               priority={true}
-              className="h-32 w-32 rounded-full object-cover object-top border-[0.35rem] border-white shadow-xl"
+              className="h-32 w-32 rounded-full border-[0.35rem] border-white object-cover object-top shadow-xl"
             />
           </motion.div>
-          <span className="absolute bottom-2 right-2 text-4xl">👋🏼</span>
+          <motion.span
+            className="absolute bottom-2 right-2 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋🏼
+          </motion.span>
         </div>
       </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        Hello, I&apos;m <span className="font-bold">Matías Roje.</span> I&apos;m
+        a <span className="font-bold">Full-Stack Developer</span> with focus in{" "}
+        <span className="font-bold">React </span>
+        <span className="italic">(Next.js, TypeScrypt, Node.js)</span> and
+        additional expertise in <span className="italic">Ruby</span> and
+        <span className="italic"> Ruby on Rails</span>.
+      </motion.h1>
+
+      <motion.div
+        className="flex flex-col items-center justify-center gap-3 px-4 text-lg font-medium sm:flex-row"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group flex items-center gap-2 rounded-full bg-gray-900 px-7 py-3 text-white outline-none transition hover:scale-110 hover:bg-gray-950 focus:scale-110 active:scale-105"
+        >
+          Contact me{" "}
+          <FontAwesomeIcon
+            icon={faArrowRight}
+            style={{ fontSize: 18 }}
+            className="opacity-80 transition group-hover:translate-x-0.5"
+          />
+        </Link>
+
+        <a
+          className="group flex cursor-pointer items-center gap-2 rounded-full border border-black/5 bg-white px-7 py-3 outline-none transition hover:scale-110 focus:scale-110 active:scale-105"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <FontAwesomeIcon
+            icon={faFileArrowDown}
+            style={{ fontSize: 18 }}
+            className="opacity-80 transition group-hover:translate-y-0.5"
+          />
+        </a>
+        <div className="flex gap-3">
+          <a
+            className="flex cursor-pointer items-center gap-2 rounded-full border border-black/5 bg-white p-4 text-gray-700 outline-none transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105"
+            href="https://www.linkedin.com/in/mat%C3%ADas-roje-carrasco-2a2673273/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: 20 }} />
+          </a>
+
+          <a
+            className="flex cursor-pointer items-center gap-2 rounded-full border border-black/5 bg-white p-4 text-gray-700 outline-none transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105"
+            href="https://github.com/MatiasRoje"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faGithub} style={{ fontSize: 20 }} />
+          </a>
+        </div>
+      </motion.div>
     </section>
   );
 }
